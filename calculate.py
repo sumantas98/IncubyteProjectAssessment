0ee977a2bbb96e83df5e2b16b2b8979b13cc84eb
Sumanta Samanta <sumantasamanta98@gmail.com>
2024-09-11 17:34:47 +0530

    Added Test Cases : 6. input("2,2,0") output(4) 7. input("2,2,3") output(7) 8. input("2,1,5,7") output(15) 9. input("8,2,1") output(11)

--- calculate.py
+++ calculate.py
@@ -2,10 +2,16 @@ import re
 def addition(random_string):
     if random_string == "" or re.match(r'^\D*$', random_string):
         return 0
+    '''
+    ### Handle only Twice number with comma ###
     if "," in random_string:
         num_list = random_string.split(",")
         return int(num_list[0]) + int(num_list[1])
-    return int(random_string)
+    '''
+
+    # This Function will handle multiple "," separated numbers like "1,2,3" out put should be 6.
+    list_num = random_string.split(",")
+    return sum(int(num) for num in list_num)
 
 
 
--- testcases.py
+++ testcases.py
@@ -11,6 +11,11 @@ class TestCalculatorNumbers(unittest.TestCase):
         self.assertEqual(calculate.addition("1"), 1)
     def test_twice_number(self):
         self.assertEqual(calculate.addition("2,2"), 4)
+    def test_multiple_number_with_comma(self):
+        self.assertEqual(calculate.addition("2,2,0"), 4)
+        self.assertEqual(calculate.addition("2,2,3"), 7)
+        self.assertEqual(calculate.addition("2,1,5,7"), 15)
+        self.assertEqual(calculate.addition("8,2,1"), 11)
 
 
 
