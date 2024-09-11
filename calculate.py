e3b254908bf0c98cd35324e68b8db097e7b33193
Sumanta Samanta <sumantasamanta98@gmail.com>
2024-09-11 17:25:34 +0530

    Added Test Cases : 3. input("*jauyw#$") output(0) 5. input("2,2") output(4)
    If we pass ,
    input("1,2") output(1) then it will through the below error ,
    
        self.assertEqual(calculate.addition("1,2"), 1)
    AssertionError: 3 != 1
    Hence, output should be proper in the test result.

--- calculate.py
+++ calculate.py
@@ -2,6 +2,9 @@ import re
 def addition(random_string):
     if random_string == "" or re.match(r'^\D*$', random_string):
         return 0
+    if "," in random_string:
+        num_list = random_string.split(",")
+        return int(num_list[0]) + int(num_list[1])
     return int(random_string)
 
 
--- testcases.py
+++ testcases.py
@@ -4,10 +4,15 @@ import calculate
 class TestCalculatorNumbers(unittest.TestCase):
     def test_empty_string(self):
         self.assertEqual(calculate.addition(""), 0)
-        self.assertEqual(calculate.addition("k"),0)
+        self.assertEqual(calculate.addition("abcd"), 0)
+        self.assertEqual(calculate.addition("*jauyw#$"), 0)
 
     def test_single_number(self):
         self.assertEqual(calculate.addition("1"), 1)
+    def test_twice_number(self):
+        self.assertEqual(calculate.addition("2,2"), 4)
+
+
 
 # This main function.
 if __name__ == '__main__':
