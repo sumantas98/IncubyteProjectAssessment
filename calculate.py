66bd1e26a021fee9de4536ddd59c21b404624d3f
Sumanta Samanta <sumantasamanta98@gmail.com>
2024-09-11 17:13:24 +0530

    README.MD Changes with valuable artifacts as mentioned below. 1. Project Architecture 2. Execution Steps. 3. Commit Information. 4. Fixed empty string with string value issue like input("abcd") output: 0 input("1") output:1

--- calculate.py
+++ calculate.py
@@ -1,6 +1,8 @@
+import re
 def addition(random_string):
-    if random_string == "":
+    if random_string == "" or re.match(r'^\D*$', random_string):
         return 0
+    return int(random_string)
 
 
 
--- testcases.py
+++ testcases.py
@@ -4,6 +4,10 @@ import calculate
 class TestCalculatorNumbers(unittest.TestCase):
     def test_empty_string(self):
         self.assertEqual(calculate.addition(""), 0)
+        self.assertEqual(calculate.addition("k"),0)
+
+    def test_single_number(self):
+        self.assertEqual(calculate.addition("1"), 1)
 
 # This main function.
 if __name__ == '__main__':
